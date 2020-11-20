94671be02905f2cd7d522993c63df4eeffe1afe1
Tony Blum <tonyblum@me.com>
2020-11-20 13:10:06 -0800

    d4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  root to: "home#index"
+   root 'welcome#index'
+  # root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
